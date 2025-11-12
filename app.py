320353affadc577637a4e9bc37a1f91394f19715
Emi00093522-cloud <00093522@uca.edu.sv>
2025-11-12 01:13:16 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import streamlit as st
 from modulos.registro_usuario import registrar_usuario
+from modulos.ahorros import mostrar_ahorros  # Importamos la función mostrar_venta del módulo venta
+from modulos.login import login
+mostrar_ahorros()
+login()
+# Llamamos a la función mostrar_venta para mostrar el mensaje en la app
+mostrar_ahorros()
 
 st.set_page_config(page_title="Sistema GAPCSV", page_icon="🧁", layout="centered")
 
