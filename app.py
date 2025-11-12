a2719ce806fb852856404ecb2bc5ad5f7ff56fb4
Emi00093522-cloud <00093522@uca.edu.sv>
2025-11-12 00:50:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-# app.py
 import streamlit as st
-from modulos.ahorros import mostrar_ahorros  # Importamos la función mostrar_venta del módulo venta
-from modulos.login import login
-# Llamamos a la función mostrar_venta para mostrar el mensaje en la app
-mostrar_ahorros()
-login()
+from modulos.registro_usuario import registrar_usuario
+
+st.set_page_config(page_title="Sistema GAPCSV", page_icon="🧁", layout="centered")
+
+menu = st.sidebar.selectbox("Menú", ["Registrar usuario", "Login"])
+
+if menu == "Registrar usuario":
+    registrar_usuario()
+elif menu == "Login":
+    st.write("Aquí irá tu formulario de login")
