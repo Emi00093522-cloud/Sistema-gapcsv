97d78f64b6b3450a4a461f2f5a024ec400373021
Emi00093522-cloud <00093522@uca.edu.sv>
2025-11-11 23:59:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 # app.py
 import streamlit as st
 from modulos.ahorro import mostrar_ahorro  # Importamos la función mostrar_venta del módulo venta
-
+from modulos.login import login
 # Llamamos a la función mostrar_venta para mostrar el mensaje en la app
 mostrar_ahorro()
+login()
 
