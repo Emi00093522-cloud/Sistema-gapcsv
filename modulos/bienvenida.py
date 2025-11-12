410cb12bd127f84eb68b62cd0ef0e437ce1af25a
ek-mtz07 <00032022@uca.edu.sv>
2025-11-12 10:04:40 -0600

    Delete modulos/bienvenida.py

--- modulos/bienvenida.py
+++
@@ -1,6 +0,0 @@
-# modulos/ahorros.py
-import streamlit as st
-def mostrar_bienvenida():
- # Aquí se muestra "Hola Mundo"
- st.title("Hola Mundo")
- st.write("¡Este es un mensaje de prueba para verificar si la app está funcionando!")
