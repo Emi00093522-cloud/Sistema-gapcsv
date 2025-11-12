00d383a7db410b404f924df276b725e99f133c89
Emi00093522-cloud <00093522@uca.edu.sv>
2025-11-11 23:50:08 -0600

    Create ahorros.py

---
+++ modulos/ahorros.py
@@ -0,0 +1,6 @@
+# modulos/ahorros.py
+import streamlit as st
+def mostrar_ahorros():
+ # Aquí se muestra "Hola Mundo"
+ st.title("Hola Mundo")
+ st.write("¡Este es un mensaje de prueba para verificar si la app está funcionando!")
