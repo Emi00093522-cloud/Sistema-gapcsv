bbb816b3573e7d720bb3eb6b6a4e5265b104bce6
ek-mtz07 <00032022@uca.edu.sv>
2025-11-12 14:05:44 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from modulos.registro_usuario import registrar_usuario
 from modulos.login import login
 # from modulos.grupos import mostrar_grupo
-mostrar_grupo()
+# mostrar_grupo()
 # ⚙️ Configuración de la app
 st.set_page_config(page_title="Sistema GAPCSV", page_icon="💜", layout="centered")
 
