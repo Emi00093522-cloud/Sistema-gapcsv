f4b4030ee740166bdc53fd5bc3bd51038234af3c
HugoAlberto-1 <healberto@uca.edu.sv>
2025-11-13 11:03:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ if st.session_state["sesion_iniciada"]:
     tipo = (st.session_state.get("tipo_usuario", "Desconocido") or "").strip().lower()
     cargo = (st.session_state.get("cargo","Desconocido")).strip().lower()
 
-    st.sidebar.write(f"👤 **{usuario}** ({cargo})")
+    st.sidebar.write(f"👤 **{usuario}** ({tipo})")
 
     # --- Si es SECRETARIA o PRESIDENTE: MENÚ REDUCIDO SOLO CON 3 OPCIONES ---
     if cargo in ("SECRETARIA", "PRESIDENTE"):
