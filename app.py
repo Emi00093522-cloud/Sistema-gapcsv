6169b5709a5a0813ad3fb385333e8f1c862169a8
HugoAlberto-1 <healberto@uca.edu.sv>
2025-11-13 11:17:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def make_menu(options_dict, default_label=None, key="menu_principal"):
 if st.session_state["sesion_iniciada"]:
     usuario = st.session_state.get("usuario", "Usuario")
     tipo = (st.session_state.get("tipo_usuario", "Desconocido") or "").strip().lower()
-    cargo = st.session_state.get("Cargo","tipo_de_cargo")
+    cargo = st.session_state.get("cargo_de_usuario","Cargo")
 
     st.sidebar.write(f"👤 **{usuario}** ({cargo})")
 
