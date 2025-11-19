0214886ec4d06e651f2e4204cb2077ce05675c2f
ek-mtz07 <00032022@uca.edu.sv>
2025-11-19 17:58:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from modulos.miembros import mostrar_miembro
 from modulos.prestamo import mostrar_prestamo
 from modulos.reuniones import mostrar_reuniones
 from modulos.asistencia import mostrar_asistencia
-from modulos.reglamentos import mostrat_reglamentos
+from modulos.reglamentos import mostrar_reglamentos
 
 # -------- CONFIG -----------
 st.set_page_config(page_title="Sistema GAPCSV", page_icon="💜", layout="wide")
