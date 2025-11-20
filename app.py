2c877e97807d4ee7ea60e3c689ed0c0c92d6c9ac
Emi00093522-cloud <00093522@uca.edu.sv>
2025-11-20 02:37:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -199,7 +199,9 @@ else:
             st.rerun()
 
     elif st.session_state["pagina_actual"] == "inicio":
-        st.title("💜 Sistema GAPCSV")
+        st.title("🟦 Sistema GAPCSV
+Grupos de Ahorro y Préstamo Comunitario
+Esta página servirá como plataforma para gestionar grupos, registrar miembros, administrar reuniones y llevar el control financiero de forma eficiente y organizada.")
         st.subheader("Grupos de Ahorro y Préstamo Comunitario")
         st.write("---")
 
