c289529b8995537b267e50d5d02e950f70208c34
Emi00093522-cloud <00093522@uca.edu.sv>
2025-11-24 17:24:18 -0600

    Update app.py

--- app.py
+++ app.py
@@ -166,7 +166,7 @@ if st.session_state["sesion_iniciada"]:
         panel_presidente()
     elif tipo == "promotora" or cargo == "PROMOTORA":
         panel_promotora(usuario)
-    elif cargo == "administrador":
+    elif cargo == "ADMINISTRADOR":
         panel_admin()
     else:
         st.error("⚠️ Tipo de usuario no reconocido.")
